95516ad5476032d3d66c499035063c50ec43fb2c
Obaida <almelhemo@gmail.com>
2025-10-05 15:21:47 -0400

    added websocker-broadcaster to send message to all members of a room and some validations in rooms-runtime

--- Tic Tac Toe/rooms-runtime.js
+++ Tic Tac Toe/rooms-runtime.js
@@ -1,4 +1,5 @@
 import { Room } from "./Room.js";
+import { broadcastToRoom } from "./websocket-broadcaster.js";
 
 // map id to object
 let rooms = new Map();
@@ -14,25 +15,44 @@ function getRoom(roomId) {
 }
 
 function newRoom(user) {
+    if (playersRooms.has(user.id)) {
+        throw new Error("Player is already in a room.");
+    }
     let room = new Room(user);
     rooms.set(room.id, room);
     playersRooms.set(user.id, room.id);
+    user.isReady = false;
+    broadcastToRoom(room.id, { type: 'playerRoomUpdate', room: room });
     return room;
 }
 
 function joinRoom(roomId, user) {
+    if (playersRooms.has(user.id)) {
+        throw new Error("Player is already in a room.");
+    }
     let room = rooms.get(roomId);
+    if (!room) {
+        throw new Error("Room not found.");
+    }
+    if (room.isPlaying) {
+        throw new Error("Cannot join a room with a game in progress.");
+    }
     room.addUser(user);
     playersRooms.set(user.id, room.id);
+    user.isReady = false;
+    broadcastToRoom(room.id, { type: 'playerRoomUpdate', room: room });
     return room;
 }
 
 function leaveRoom(user) {
     let room = getPlayerRoom(user.id);
     room.removeUser(user);
+    
     playersRooms.delete(user.id);
     if (Object.keys(room.users).length == 0) {
-        rooms.delete(roomId)
+        rooms.delete(room.id);
+    } else {
+        broadcastToRoom(room.id, { type: 'playerRoomUpdate', room: room });
     }
 }
 
@@ -49,12 +69,14 @@ function setPlayerReady(user, isReady) {
     if (players.length == 2 && players.every(player => player.isReady == true)) {
         room.startGame();
     }
+    broadcastToRoom(room.id, { type: 'playerRoomUpdate', room: room });
 }
 
 function makeMove(user, move) {
     let room = getPlayerRoom(user.id)
     if (room.isPlaying == true) {
         room.game.playMove(user, move);
+        broadcastToRoom(room.id, { type: 'playerRoomUpdate', room: room });
     } else {
         throw new Error("No game has started")
     }
@@ -62,6 +84,7 @@ function makeMove(user, move) {
 
 function getPlayerRoom(userId) {
     let roomId = playersRooms.get(userId);
+
     if(roomId == null){
         throw new Error("Player is not in any room");
     }
